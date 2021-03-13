9ac5158b0072a0f89f1c43aac9f8d9c687297e12
Sarifou Diallo <mamadousarifoudiallo02@gmail.com>
2021-03-13 20:23:34 +0100

    Personnalisation du header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,36 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectComponent } from './project/project.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { LearningComponent } from './learning/learning.component';
+import { CertificationComponent } from './certification/certification.component';
+
+const appRoutes : Routes = [
+  {path: 'home', component : HomeComponent},
+  {path: 'project', component : ProjectComponent},
+  {path: 'certification', component : CertificationComponent},
+  {path: 'learning', component : LearningComponent},
+  {path: 'experience', component : ExperienceComponent},
+  {path: 'contact', component : ContactComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    ProjectComponent
+    ProjectComponent,
+    ContactComponent,
+    ExperienceComponent,
+    LearningComponent,
+    CertificationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
