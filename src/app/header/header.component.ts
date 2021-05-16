b6200849e873f2634d0c0ed346fb3f57f89a6875
Sarifou Diallo <mamadousarifoudiallo02@gmail.com>
2021-05-16 21:31:07 +0200

    Adding smtp service for send mail

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalService } from '../global-variable/global.service';
 import { Subscription } from 'rxjs';
-
+import './../../assets/smtp.js';
+declare let Email : any ;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,7 +12,22 @@ export class HeaderComponent implements OnInit {
   head : boolean = false ;
   display : boolean = false ;
   headSubs: Subscription = new Subscription;
-  constructor(private headService : GlobalService) { }
+
+  constructor(private headService : GlobalService) {
+    //console.log(Email);
+    Email.send({
+      Host : 'smtp.elasticemail.com',
+      Username : 'cherif@sarifou.com',
+      Password : '75BFB505932E7D7A165CAC848642B5AE1F85',
+      To : 'mamadousarifoudiallo63@gmail.com',
+      From : 'diallo.msdpro@gmail.com',
+      Subject : 'Prise de contact',
+      Body : 'blabla'
+      }).then( (message: any)  => { 
+        console.log(message)
+    } );
+        
+  }
 
   ngOnInit(): void {
     this.headSubs = this.headService.headSubj.subscribe( (head : boolean) => {
@@ -21,5 +37,6 @@ export class HeaderComponent implements OnInit {
   onDisplay() {
     this.display = ! this.display;
     console.log(this.display);
+    console.log(Email);
   }
 }
